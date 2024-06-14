f4da8b5c6be18a3d4adfbd585d2631206ee0798f
AtheekJAR <contactatheekjar@gmail.com>
2024-06-15 02:42:03 +0530

    fix (Bug) :  when the user enters an invalid choice the code didn't work as expected. to resolve this behaviour playRound function was updated!

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let randomNumber = Math.random()
-    if(randomNumber <= 1/3) {
+    if (randomNumber <= 1 / 3) {
         return "rock"
-    }else if(randomNumber >= 1/3 && randomNumber <= 2/3) {
+    } else if (randomNumber >= 1 / 3 && randomNumber <= 2 / 3) {
         return "paper"
-    }else {
+    } else {
         return "scissors"
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let choice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
-    
-    if(choice === "rock" || choice === "paper" || choice === "scissors"){
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice
-    }else{
+    } else {
         console.log("Invalid choice. Please enter rock, paper, or scissors.");
+        return;
     }
 }
 
@@ -23,34 +24,43 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if(humanChoice === computerChoice){
+    if (humanChoice === computerChoice) {
         humanScore++;
         computerScore++;
         console.log("It's a tie! Both chose " + humanChoice);
-    }else if(humanChoice === 'rock') {
-        if(computerChoice === 'scissors'){
+    } else if (humanChoice === 'rock') {
+        if (computerChoice === 'scissors') {
             humanScore++;
             console.log("You win! Rock beats scissors.");
-        }else {
+        } else {
             computerScore++;
             console.log("You lose! Paper beats rock.");
         }
-    }else if(humanChoice === 'paper') {
-        if(computerChoice === 'rock'){
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') {
             humanScore++;
             console.log("You win! Paper beats rock.");
-        }else {
+        } else {
             computerScore++;
             console.log("You lose! Scissors beats paper.");
         }
-    }else{
-        if(computerChoice === 'paper'){
+    } else if (humanChoice === 'scissors') {
+        if (computerChoice === 'paper') {
             humanScore++;
             console.log("You win! scissors beats paper.");
-        }else{
+        } else {
             computerScore++;
             console.log("You lose! Rock beats scissors.");
         }
-    }
-       
+    } else { // this block will handle if user enters a invalid choice
+
+    } 
+
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(`human score: ${humanScore} computer score: ${computerScore}`);
